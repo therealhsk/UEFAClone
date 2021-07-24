8a601d44129e708db0dfd33c7842cebdddd32f53
Hasan Shahid <50989950+therealhsk@users.noreply.github.com>
2021-07-24 18:49:37 +0500

    Select players and limit them according to formation (#3)
    atom for number of players and total price

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-import TabOneScreen from './screens/TabOneScreen';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import TabOneScreen from "./screens/TabOneScreen";
+
+import { RecoilRoot } from "recoil";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -17,7 +19,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <TabOneScreen />
+        <RecoilRoot>
+          <TabOneScreen />
+        </RecoilRoot>
         <StatusBar />
       </SafeAreaProvider>
     );
